ba5c4e705f26e7e3fd58bc69d4cbab572ea92cab
Abr0sh <135833676+Abr0sh@users.noreply.github.com>
2023-09-27 09:39:12 +0200

    Update and rename world.py to world_class.py

--- world_class.py
+++ world_class.py
@@ -23,11 +23,26 @@ class World:
 
         self.population_count = population_count # a list with the number of people in each nation ex: [100,200,40, . . . ]
         self.total_population_count = total_count # total number of people in the world
+        self.restrictions = False
+        self.disease = False
+
+    def add_restrictions(self,TR,CR):
+        self.restrictions = True
+        self.travel_restriction_function = TR
+        self.contact_restricion_function = CR
+
+    def add_dissease(self,alpha,beta,gamma,name):
+        self.alpha = np.array(alpha)
+        self.beta = np.array(beta)
+        self.gamma = np.array(gamma)
+        self.disease_name = name
+        self.disease = True
 
 
-    def forcast_SIS(self,alpha,beta,initial_conditions,T_final,dt,odesolver = "RK"):
-        if self.nr_of_nations != 1: raise ValueError("Number of nations must be 1 for this SIS model") # assumes one nation world 
-        N = self.total_population_count
+    def forcast_SIS(self,initial_conditions,T_final,dt,odesolver = "RK",k = 0):
+        # add check for k and alpha beta
+        alpha,beta = self.alpha[k],self.beta[k]
+        N = self.population_count[k]
         t = np.arange(0,T_final,dt)
         def f(t,y):
             # The right hand function of the ODE dy/dt = f(t,y)
@@ -42,10 +57,9 @@ class World:
         sol,t = mysolver.solve(t)
         return sol , t
 
-    def forcast_SIR(self,alpha,beta,gamma,initial_conditions,T_final,dt,odesolver = "RK"):
-        if self.nr_of_nations != 1: raise ValueError("Number of nations must be 1 for this SIS model") # assumes one nation world 
-        # alpha: recovery rate , beta: infection rate  , gamma: vaccinatin rate
-        N = self.total_population_count 
+    def forcast_SIR(self,initial_conditions,T_final,dt,odesolver = "RK",k = 0):
+        alpha,beta,gamma = self.alpha[k],self.beta[k],self.gamma[k]
+        N = self.population_count[k]
         def f(t,y):
             # Defines the right hand function of the ODE dy/dt = f(t,y)
             S,I,R = y
@@ -61,23 +75,24 @@ class World:
         return sol ,t
     
     
-    def forcast_SIRT(self,alpha,beta,gamma,Travel_matrix,initial_conditions,T_final,dt):
+    def forcast_SIRT(self,initial_conditions,T_final,dt,Travel_matrix,odesolver = "RK"):
         # SIR model with traveling 
+        alpha,beta,gamma = self.alpha,self.beta,self.gamma
         nr_of_nations = self.nr_of_nations
         N = self.population_count
         T = Travel_matrix
 
         def f1(t,y):
             S,I,R = y
-            return np.array([-beta*S*I/N -gamma*S, beta*S*I/N -alpha*I, gamma*S+alpha*I])
+            return np.array([-beta[0]*S*I/N -gamma[0]*S, beta[0]*S*I/N -alpha[0]*I, gamma[0]*S+alpha[0]*I])
         def f(t,SIR_vect):
             # Expect a vector of the form [S1, S2, S3, . . , I1,I2,I3, . . . , R1,R2,R3, . . . ]
             f_vect = np.zeros(3*nr_of_nations)
             S,I,R = np.split(SIR_vect,len(SIR_vect)//nr_of_nations)[0],np.split(SIR_vect,len(SIR_vect)//nr_of_nations)[1],np.split(SIR_vect,len(SIR_vect)//nr_of_nations)[2]
             for i in range(nr_of_nations):
-                f_vect[i] = -beta*S[i]*I[i]/N[i] -gamma*S[i] + np.dot(T[:,i],S)-sum(T[i])*S[i]
-                f_vect[nr_of_nations+i] = beta*S[i]*I[i]/N[i] - alpha*I[i] + np.dot(T[:,i],I) - sum(T[i])*I[i]
-                f_vect[2*nr_of_nations+i] = gamma*S[i] + alpha*I[i] +np.dot(T[:,i],R) - sum(T[i])*R[i]
+                f_vect[i] = -beta[i]*S[i]*I[i]/N[i] -gamma[i]*S[i] + np.dot(T[:,i],S)-sum(T[i])*S[i]
+                f_vect[nr_of_nations+i] = beta[i]*S[i]*I[i]/N[i] - alpha[i]*I[i] + np.dot(T[:,i],I) - sum(T[i])*I[i]
+                f_vect[2*nr_of_nations+i] = gamma[i]*S[i] + alpha[i]*I[i] +np.dot(T[:,i],R) - sum(T[i])*R[i]
             return f_vect
         if nr_of_nations ==1: odesolver = RungeKutta45(f1)
         else: odesolver = RungeKutta45(f)
